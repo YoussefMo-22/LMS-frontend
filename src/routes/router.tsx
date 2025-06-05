d3ac60fb5bc5e084d9fff9a8d9c612515317ea27
Youssef <youssefji3mohamed33@gmail.com>
2025-06-05 03:34:40 +0300

    handle route  error

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,7 +3,6 @@
 // // import Home from "../pages/Home";
 
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import RootLayout from "../app/RootLayout";
 import LoginPage from "../features/auth/Login";
 import RegisterPage from "../features/auth/register";
 import ForgetPassword from "../features/auth/ForgetPassword";
@@ -16,8 +15,6 @@ const router = createBrowserRouter(
         <Route index path="/forgot-password" element={<ForgetPassword />}/>
         <Route index path="/reset-password" element={<ResetPassword />}/>
         <Route path="/register" element={<RegisterPage />}/>
-        <Route path="/" element={<RootLayout />}>
-        </Route>
         </>
     )
 )
